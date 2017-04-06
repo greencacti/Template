b2818756879b7331b0480ecdff0fc2f036298fe7
Benjamin Wang <baominw@baominw-m01.vmware.com>
2017-04-06 14:26:24 +0800

    logback support

--- src/main/java/com/greencacti/TemplateApplication.java
+++ src/main/java/com/greencacti/TemplateApplication.java
@@ -1,5 +1,7 @@
 package com.greencacti;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,7 +10,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  */
 @SpringBootApplication
 public class TemplateApplication {
+    private static final Logger logger = LoggerFactory.getLogger(TemplateApplication.class);
+
     public static void main(String[] args){
+        logger.debug("Template application is started.");
         SpringApplication.run(TemplateApplication.class, args);
     }
 }
