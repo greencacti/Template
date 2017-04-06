c5b7ff595cc59d9e19e3ac273440b9a556e82df9
Benjamin Wang <baominw@baominw-m01.vmware.com>
2017-04-06 14:45:28 +0800

    JPA support: Part1

---
+++ src/main/java/com/greencacti/domain/Person.java
@@ -0,0 +1,35 @@
+package com.greencacti.domain;
+
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+/**
+ * Created by baominw on 4/6/17.
+ */
+@Entity
+public class Person  implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private int age;
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+}
